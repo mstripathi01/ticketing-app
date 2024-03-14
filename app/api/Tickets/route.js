4d909a3d0d54188b8d86caff27f5ff1b579f2874
mstripathi01 <mayank.tripathi218@gmail.com>
2024-03-15 00:03:17 +0530

    ticketing-app

---
+++ app/api/Tickets/route.js
@@ -0,0 +1,28 @@
+import { NextResponse } from "next/server";
+import Ticket from "@/app/models/Ticket";
+
+export async function GET() {
+  try {
+    const tickets = await Ticket.find();
+
+    return NextResponse.json({ tickets }, { status: 200 });
+  } catch (err) {
+    console.log(err);
+    return NextResponse.json({ message: "Error", err }, { status: 500 });
+  }
+}
+
+export async function POST(req) {
+  console.log(req, "req");
+  try {
+    const body = await req.json();
+    const ticketData = body.formData;
+
+    await Ticket.create(ticketData);
+
+    return NextResponse.json({ message: "Ticket Created" }, { status: 201 });
+  } catch (err) {
+    console.log(err);
+    return NextResponse.json({ message: "Error", err }, { status: 500 });
+  }
+}
